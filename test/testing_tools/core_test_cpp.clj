d0847cc6b6f49462f64e3750cff2e48cf0600ccb
valentin-shipilov <valentin.shipilov@gmail.com>
2016-09-22 06:06:05 +0300

    Buitify

--- test/testing_tools/core_test_cpp.clj
+++ test/testing_tools/core_test_cpp.clj
@@ -2,38 +2,44 @@
   (:require [clojure.test :refer :all]
             [testing-tools.core :refer :all]))
 
+(def lang :cpp)
+
 (deftest assert-true-test
   (testing "Should return ASSERT_TRUE(true)."
-    (is (= "ASSERT_TRUE(true)" (generate-assert :cpp '(assert true))))))
+    (is (= "ASSERT_TRUE(true)" (generate-assert lang '(assert true))))))
 
 (deftest assert-false-test
   (testing "Should return ASSERT_TRUE(false)."
-    (is (= "ASSERT_TRUE(false)" (generate-assert :cpp '(assert false))))))
+    (is (= "ASSERT_TRUE(false)" (generate-assert lang '(assert false))))))
 
 (deftest assert-is-prime-of-number
   (testing "Should return ASSERT_TRUE(is_prime(10))"
-    (is (= "ASSERT_TRUE(is_prime(10))" (generate-assert :cpp '(assert (is_prime 10)))))))
+    (is (= "ASSERT_TRUE(is_prime(10))" (generate-assert lang '(assert (is_prime 10)))))))
 
 (deftest assert-some-funct-of-many-args
   (testing "Should return ASSERT_TRUE(some_funct(10, 11, 12))"
-    (is (= "ASSERT_TRUE(some_funct(10, 11, 12))" (generate-assert :cpp '(assert (some_funct 10 11 12)))))))
+    (is (= "ASSERT_TRUE(some_funct(10, 11, 12))" (generate-assert lang '(assert (some_funct 10 11 12)))))))
 
 (deftest assert-is-prime-of-sum
   (testing "Should return ASSERT_TRUE(is_prime(10 + 1)))"
-    (is (= "ASSERT_TRUE(is_prime(10 + 1))" (generate-assert :cpp '(assert (is_prime (+ 10 1))))))))
+    (is (= "ASSERT_TRUE(is_prime(10 + 1))" (generate-assert lang '(assert (is_prime (+ 10 1))))))))
 
 (deftest assert-is-prime-of-substraction
   (testing "Should return ASSERT_TRUE(is_prime(10 - 1)))"
-    (is (= "ASSERT_TRUE(is_prime(10 - 1))" (generate-assert :cpp '(assert (is_prime (- 10 1))))))))
+    (is (= "ASSERT_TRUE(is_prime(10 - 1))" (generate-assert lang '(assert (is_prime (- 10 1))))))))
 
 (deftest assert-is-prime-of-multiplication
   (testing "Should return ASSERT_TRUE(is_prime(10 * 1)))"
-    (is (= "ASSERT_TRUE(is_prime(10 * 1))" (generate-assert :cpp '(assert (is_prime (* 10 1))))))))
+    (is (= "ASSERT_TRUE(is_prime(10 * 1))" (generate-assert lang '(assert (is_prime (* 10 1))))))))
 
 (deftest assert-is-prime-of-division
   (testing "Should return ASSERT_TRUE(is_prime(10 / 1)))"
-    (is (= "ASSERT_TRUE(is_prime(10 / 1))" (generate-assert :cpp '(assert (isprime (/ 10 1))))))))
+    (is (= "ASSERT_TRUE(is_prime(10 / 1))" (generate-assert lang '(assert (is_prime (/ 10 1))))))))
 
 (deftest assert-is-prime-of-sum-foo-bar
   (testing "ASSERT_TRUE(is_prime(foo(10, 11) + bar(1, 2 - 3)))"
-    (is (= "ASSERT_TRUE(is_prime(foo(10, 11) + bar(1, 2 - 3)))" (generate-assert :cpp '(assert (is_prime (+ (foo 10 11) (bar 1 (- 2 3))))))))))
+    (is (= "ASSERT_TRUE(is_prime(foo(10, 11) + bar(1, 2 - 3)))" (generate-assert lang '(assert (is_prime (+ (foo 10 11) (bar 1 (- 2 3))))))))))
+
+(deftest assert-test-of-reminder
+  (testing "assertTrue(test(10 % 2))"
+    (is (= "ASSERT_TRUE(test(10 % 2))" (generate-assert lang '(assert (test (rem 10 2))))))))
--- test/testing_tools/core_test_java.clj
+++ test/testing_tools/core_test_java.clj
@@ -2,42 +2,44 @@
   (:require [clojure.test :refer :all]
             [testing-tools.core :refer :all]))
 
+(def lang :java)
+
 (deftest assert-true-test
   (testing "Should return assertTrue(true)."
-    (is (= "assertTrue(true)" (generate-assert :java '(assert true))))))
+    (is (= "assertTrue(true)" (generate-assert lang '(assert true))))))
 
 (deftest assert-false-test
   (testing "Should return assertTrue(false)."
-    (is (= "assertTrue(false)" (generate-assert :java '(assert false))))))
+    (is (= "assertTrue(false)" (generate-assert lang '(assert false))))))
 
 (deftest assert-is-prime-of-number
   (testing "Should return assertTrue(is_prime(10))"
-    (is (= "assertTrue(is_prime(10))" (generate-assert :java '(assert (is_prime 10)))))))
+    (is (= "assertTrue(is_prime(10))" (generate-assert lang '(assert (is_prime 10)))))))
 
 (deftest assert-some-funct-of-many-args
   (testing "Should return assertTrue(some_funct(10, 11, 12))"
-    (is (= "assertTrue(some_funct(10, 11, 12))" (generate-assert :java '(assert (some_funct 10 11 12)))))))
+    (is (= "assertTrue(some_funct(10, 11, 12))" (generate-assert lang '(assert (some_funct 10 11 12)))))))
 
 (deftest assert-is-prime-of-sum
   (testing "Should return assertTrue(is_prime(10 + 1)))"
-    (is (= "assertTrue(is_prime(10 + 1))" (generate-assert :java '(assert (is_prime (+ 10 1))))))))
+    (is (= "assertTrue(is_prime(10 + 1))" (generate-assert lang '(assert (is_prime (+ 10 1))))))))
 
 (deftest assert-is-prime-of-substraction
   (testing "Should return assertTrue(is_prime(10 - 1)))"
-    (is (= "assertTrue(is_prime(10 - 1))" (generate-assert :java '(assert (is_prime (- 10 1))))))))
+    (is (= "assertTrue(is_prime(10 - 1))" (generate-assert lang '(assert (is_prime (- 10 1))))))))
 
 (deftest assert-is-prime-of-multiplication
   (testing "Should return assertTrue(is_prime(10 * 1)))"
-    (is (= "assertTrue(is_prime(10 * 1))" (generate-assert :java '(assert (is_prime (* 10 1))))))))
+    (is (= "assertTrue(is_prime(10 * 1))" (generate-assert lang '(assert (is_prime (* 10 1))))))))
 
 (deftest assert-is-prime-of-division
   (testing "Should return assertTrue(is_prime(10 / 1)))"
-    (is (= "assertTrue(is_prime(10 / 1))" (generate-assert :java '(assert (is_prime (/ 10 1))))))))
+    (is (= "assertTrue(is_prime(10 / 1))" (generate-assert lang '(assert (is_prime (/ 10 1))))))))
 
 (deftest assert-is-prime-of-sum-foo-bar
   (testing "assertTrue(is_prime(foo(10, 11) + bar(1, 2 - 3)))"
-    (is (= "assertTrue(is_prime(foo(10, 11) + bar(1, 2 - 3)))" (generate-assert :java '(assert (is_prime (+ (foo 10 11) (bar 1 (- 2 3))))))))))
+    (is (= "assertTrue(is_prime(foo(10, 11) + bar(1, 2 - 3)))" (generate-assert lang '(assert (is_prime (+ (foo 10 11) (bar 1 (- 2 3))))))))))
 
 (deftest assert-test-of-reminder
   (testing "assertTrue(test(10 % 2))"
-    (is (= "assertTrue(test(10 % 2))" (generate-assert :java '(assert (test (rem 10 2))))))))
+    (is (= "assertTrue(test(10 % 2))" (generate-assert lang '(assert (test (rem 10 2))))))))
